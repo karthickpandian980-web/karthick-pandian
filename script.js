f758763f3f153ce1b01daef213dd922d982a934d
karthickpandian <karthickpandian980@gmail.com>
2026-07-02 19:16:22 +0530

    Add event listener for contact form submission

---
+++ script.js
@@ -0,0 +1,6 @@
+document
+  .getElementById("contact-form")
+  .addEventListener("submit", function (e) {
+    e.preventDefault();
+    alert("Thanks for reaching out! I’ll get back to you soon.");
+  });
